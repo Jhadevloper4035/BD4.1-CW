0eb2200b9857aee0c1c91b504c59503d96a2ee51
Jhadevloper4035 <navrojjha21@gmail.com>
2024-10-14 01:16:15 +0530

    Done all the work

--- index.js
+++ index.js
@@ -8,11 +8,54 @@ const port = 3010;
 app.use(cors());
 app.use(express.json());
 
-// let db;
+let db;
 
-// (async () => {
-//   db = await open({ ''})
-// })
+async function initDb() {
+     db = await open({
+      filename: 'database.sqlite',
+      driver: sqlite3.Database
+    });
+    return db;
+  }
+
+initDb();
+
+
+const fetchAllMovies = async () => { 
+let query = "SELECT * FROM movies"
+let response = await db.all(query , [])
+return { movies: response };
+}
+
+async function fetchMovieByGenre(genre) { 
+  let query = 'SELECT * FROM movies WHERE genre = ?';
+  let response = await db.all(query , [genre])
+  return { movie: response };
+  }
+
+
+app.get('/' , async (req,res) => {
+  const result = await fetchAllMovies()
+  res.status(200).json(result)
+})
+
+
+app.get('/movies/genre/:genre ' , async (req,res) => {
+  let genre = req.params.genre;
+  const result = await fetchMovieByGenre(genre)
+  res.status(200).json(result)
+})
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
